035b8722f540f36bb41b4caaf3c53eee31981a3e
thalles <thallesgabriel1307@gmail.com>
2022-04-13 19:43:15 -0300

    feat: add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -348,6 +348,10 @@ module.exports = {
       DEFAULT: '1'
     },
     fontFamily: {
+      regular: ['RobotoRegular'],
+      medium: ['RobotoMedium'],
+      bold: ['RobotoBold'],
+      black: ['RobotoBlack'],
       sans: [
         'ui-sans-serif',
         'system-ui',
