dcbb7a4909f70722f3c8eb3fcaecbf4093bbd514
thalles <thallesgabriel1307@gmail.com>
2022-04-14 19:02:14 -0300

    refactor: using spread operator with custom palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
+const colors = require('tailwindcss/colors')
+const palette = require('./src/assets/palette')
+
 module.exports = {
-  content: [],
+  content: ['./src/**/*.vue'],
   presets: [],
   darkMode: 'media', // or 'class'
   theme: {
@@ -10,35 +13,10 @@ module.exports = {
       xl: '1280px',
       '2xl': '1536px'
     },
-    colors: ({ colors }) => ({
-      inherit: colors.inherit,
-      current: colors.current,
-      transparent: colors.transparent,
-      black: colors.black,
-      white: colors.white,
-      slate: colors.slate,
-      gray: colors.gray,
-      zinc: colors.zinc,
-      neutral: colors.neutral,
-      stone: colors.stone,
-      red: colors.red,
-      orange: colors.orange,
-      amber: colors.amber,
-      yellow: colors.yellow,
-      lime: colors.lime,
-      green: colors.green,
-      emerald: colors.emerald,
-      teal: colors.teal,
-      cyan: colors.cyan,
-      sky: colors.sky,
-      blue: colors.blue,
-      indigo: colors.indigo,
-      violet: colors.violet,
-      purple: colors.purple,
-      fuchsia: colors.fuchsia,
-      pink: colors.pink,
-      rose: colors.rose
-    }),
+    colors: {
+      ...colors,
+      ...palette
+    },
     columns: {
       auto: 'auto',
       1: '1',
