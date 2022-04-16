5f5b2afea403471e90a6d2c4ec88cdc5f8574fe5
thalles <thallesgabriel1307@gmail.com>
2022-04-16 19:25:11 -0300

    refactor: create structure at home

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,3 +1,27 @@
 <template>
-  <h1 class="text-brand-main">Home</h1>
+  <custom-header />
+  <contact />
+  <footer class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">feedbacker &copy; {{ currentYear }}</p>
+  </footer>
 </template>
+
+<script setup lang="ts">
+import { computed, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import CustomHeader from './CustomHeader.vue'
+import Contact from './Contact.vue'
+
+const router = useRouter()
+const currentYear = computed(() => new Date().getFullYear())
+
+onMounted(() => {
+  const token = localStorage.getItem('token')
+
+  if (token) {
+    router.push({
+      name: 'Feedbacks'
+    })
+  }
+})
+</script>
