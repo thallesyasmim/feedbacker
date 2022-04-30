1ee64d7df077330102499d94905e4d6188b2c0fe
thalles <thallesgabriel1307@gmail.com>
2022-04-30 13:47:33 -0300

    feat: handle login event

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <custom-header />
+  <custom-header @login="handleLogin" />
   <contact />
   <footer class="flex justify-center py-10 bg-brand-gray">
     <p class="font-medium text-center text-gray-800">feedbacker &copy; {{ currentYear }}</p>
@@ -9,10 +9,13 @@
 <script setup lang="ts">
 import { computed, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
-import CustomHeader from './CustomHeader.vue'
+import { useModal } from '@/composables/useModal'
+import { EmitterSingleton } from '@/utils/helpers/emitter'
 import Contact from './Contact.vue'
+import CustomHeader from './CustomHeader.vue'
 
 const router = useRouter()
+const modal = useModal(new EmitterSingleton().getInstance())
 const currentYear = computed(() => new Date().getFullYear())
 
 onMounted(() => {
@@ -24,4 +27,10 @@ onMounted(() => {
     })
   }
 })
+
+function handleLogin () {
+  modal.open({
+    component: 'ModalLogin'
+  })
+}
 </script>
