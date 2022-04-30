e068d9e4fdb478972ad319bfcca3eb88f39fa959
thalles <thallesgabriel1307@gmail.com>
2022-04-30 15:04:18 -0300

    refactor: stop propagation of click

--- src/components/ModalFactory/index.vue
+++ src/components/ModalFactory/index.vue
@@ -2,12 +2,13 @@
   <teleport to="body">
     <div
       v-if="state.isActive"
-      class="fixed top-0 left-0 z-50 items-center justify-center w-full h-full bg-black bg-opacity-50"
+      class="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full bg-black bg-opacity-50"
       @click="handleModalToggle({ status: false })"
     >
       <div
         class="fixed mx-10"
         :class="DEFAULT_WIDTH"
+        @click.stop
       >
         <div class="flex flex-col overflow-hidden bg-white rounded-lg animate__animated animate__fadeInDown animate__faster">
           <div class="flex flex-col px-12 py-10 bg-white">
@@ -48,9 +49,15 @@ onBeforeUnmount(() => {
   modal.off(handleModalToggle)
 })
 
-function handleModalToggle ({ status, ...payload }: ModalTogglePayload) {
-  if (!status) {
-    state.component = undefined
+function handleModalToggle (payloads: ModalTogglePayload[] | ModalTogglePayload) {
+  let payload = payloads
+
+  if (Array.isArray(payload)) {
+    payload = payload.shift() as ModalTogglePayload
+  }
+  state.isActive = payload.status
+  if (!payload.status) {
+    state.component = {}
     state.props = {}
     state.width = DEFAULT_WIDTH
     return
@@ -58,6 +65,5 @@ function handleModalToggle ({ status, ...payload }: ModalTogglePayload) {
   state.component = payload.component
   state.props = payload.props
   state.width = payload.width ?? DEFAULT_WIDTH
-  state.isActive = status
 }
 </script>
