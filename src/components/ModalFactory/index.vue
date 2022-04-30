8a75e4f875d744b6f6c61bd18df5c16059905160
thalles <thallesgabriel1307@gmail.com>
2022-04-30 13:42:45 -0300

    feat: use modal composable and handle modal toggle

--- src/components/ModalFactory/index.vue
+++ src/components/ModalFactory/index.vue
@@ -1,8 +1,9 @@
 <template>
   <teleport to="body">
     <div
-    v-if="state.isActive"
-    class="fixed top-0 left-0 z-50 items-center justify-center w-full h-full bg-black bg-opacity-50">
+      v-if="state.isActive"
+      class="fixed top-0 left-0 z-50 items-center justify-center w-full h-full bg-black bg-opacity-50"
+    >
       <div class="fixed mx-10">
         <div class="flex flex-col overflow-hidden bg-white rounded-lg animate__animated animate__fadeInDown animate__faster">
           <div class="flex flex-col px-12 py-10 bg-white">
@@ -12,23 +13,47 @@
       </div>
     </div>
   </teleport>
-
 </template>
 
 <script setup lang="ts">
-import { reactive, Component } from 'vue'
-
-const DEFAULT_WIDTH = 'w-3/4 lg:w-1/3'
+import { reactive, onMounted, onBeforeUnmount, defineAsyncComponent, Component } from 'vue'
+import { useModal, DEFAULT_WIDTH } from '@/composables/useModal'
+import { EmitterSingleton } from '@/utils/helpers/emitter'
+import type { ModalTogglePayload } from '@/types/events'
+const ModalLogin = defineAsyncComponent(() => import('@/components/ModalLogin/index.vue'))
 
 interface IState {
   isActive: boolean
-  component: Component
+  component?: Component
   width: string
+  props?: any
 }
 
+const modal = useModal(new EmitterSingleton().getInstance())
 const state = reactive<IState>({
   isActive: false,
   component: {},
   width: DEFAULT_WIDTH
 })
+
+onMounted(() => {
+  modal.listen(handleModalToggle)
+})
+
+onBeforeUnmount(() => {
+  modal.off(handleModalToggle)
+})
+
+function handleModalToggle ({ status, ...payload }: ModalTogglePayload) {
+  if (!status) {
+    state.component = undefined
+    state.props = {}
+    state.width = DEFAULT_WIDTH
+    return
+  }
+
+  state.component = payload.component
+  state.props = payload.props
+  state.width = payload.width ?? DEFAULT_WIDTH
+}
 </script>
