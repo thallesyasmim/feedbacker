5a69f91f01e88ed3b80e35e2b55ce031a6b331a5
thalles <thallesgabriel1307@gmail.com>
2022-05-03 12:59:01 -0300

    chore: define state structure with reactive

--- src/components/ModalLogin/index.vue
+++ src/components/ModalLogin/index.vue
@@ -2,4 +2,19 @@
   <h1>Modal Login</h1>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+const state = reactive({
+  hasErrors: false,
+  isLoading: false,
+  email: {
+    value: '',
+    errorMessage: '',
+  },
+  password: {
+    value: '',
+    errorMessage: '',
+  },
+})
+</script>
