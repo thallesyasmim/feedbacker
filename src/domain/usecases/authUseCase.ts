0e69891de0f67e8ff16a4f62e63deb7a8a532cba
thalles <thallesgabriel1307@gmail.com>
2022-05-08 14:07:47 -0300

    chore: use LoginDTO type

--- src/domain/usecases/authUseCase.ts
+++ src/domain/usecases/authUseCase.ts
@@ -1,6 +1,7 @@
 import {
   AuthRepository,
   AuthRepositoryFactory,
+  LoginDTO,
 } from '@/infra/repositories/authRepository'
 
 export class AuthUseCase {
@@ -10,7 +11,7 @@ export class AuthUseCase {
     this.authRepository = authRepository
   }
 
-  async login(email: string, password: string): Promise<string> {
+  async login(email: string, password: string): Promise<LoginDTO> {
     const token = await this.authRepository.login(email, password)
 
     return token
