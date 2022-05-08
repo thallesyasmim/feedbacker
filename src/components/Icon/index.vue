40327f0e9ba380b2789d8274cc430a8a1862151b
thalles <thallesgabriel1307@gmail.com>
2022-05-08 15:19:20 -0300

    chore: create Icon component factory

---
+++ src/components/Icon/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <component :is="name" v-bind="$props" />
+</template>
+
+<script lang="ts">
+import Loading from './Loading.vue'
+
+export default {
+  components: {
+    Loading,
+  },
+  props: {
+    name: { type: String, required: true },
+  },
+}
+</script>
