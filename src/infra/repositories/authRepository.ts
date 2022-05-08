0fcc5be3e98074be551883a5f8d7ef5cc20de4d6
thalles <thallesgabriel1307@gmail.com>
2022-05-08 13:19:40 -0300

    refactor: remove try/catch block

--- src/infra/repositories/authRepository.ts
+++ src/infra/repositories/authRepository.ts
@@ -1,25 +1,28 @@
+import { httpClient } from '@/utils/helpers/httpClient'
 import { AxiosInstance } from 'axios'
 
 export class AuthRepository {
   private httpClient: AxiosInstance
-  private authLoginRoute = 'auth/login'
+  private authLoginRoute = '/auth/login'
 
   constructor(httpClient: AxiosInstance) {
     this.httpClient = httpClient
   }
 
   async login(email: string, password: string) {
-    try {
-      const {
-        data: { token },
-      } = await this.httpClient.post(this.authLoginRoute, {
-        email,
-        password,
-      })
+    const {
+      data: { token },
+    } = await this.httpClient.post(this.authLoginRoute, {
+      email,
+      password,
+    })
 
-      return token
-    } catch (error) {
-      console.error(error)
-    }
+    return token
+  }
+}
+
+export class AuthRepositoryFactory {
+  static getInstance() {
+    return new AuthRepository(httpClient)
   }
 }
