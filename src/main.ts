ad518e27a92c57e64c29076163f7e1e2ae877546
thalles <thallesgabriel1307@gmail.com>
2022-05-08 14:31:36 -0300

    chore: use vue-toastification

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Toast, { POSITION } from 'vue-toastification'
 import 'animate.css'
+import 'vue-toastification/dist/index.css'
 import '@/assets/css/global.css'
 
 const app = createApp(App)
 app.use(router)
+app.use(Toast, { position: POSITION.BOTTOM_CENTER })
 app.mount('#app')
