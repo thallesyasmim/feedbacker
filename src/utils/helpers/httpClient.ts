31f286456bcea235110fca35b63cdbe6b4694e79
thalles <thallesgabriel1307@gmail.com>
2022-05-08 13:23:18 -0300

    chore: use VUE_APP_BASE_URL env

--- src/utils/helpers/httpClient.ts
+++ src/utils/helpers/httpClient.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const httpClient = axios.create({
-  baseURL: process.env.BASE_URL || 'http://localhost:3333',
+  baseURL: process.env.VUE_APP_BASE_URL || 'http://localhost:3333',
 })
