26af1bc9deb99d1af3a539efc5d72cad779d222e
thalles <thallesgabriel1307@gmail.com>
2022-05-09 18:04:07 -0300

    chore: create nameValidator

--- src/utils/helpers/validators.ts
+++ src/utils/helpers/validators.ts
@@ -3,6 +3,14 @@ import { email, helpers, minLength, required } from '@vuelidate/validators'
 export const requiredValidator = () =>
   helpers.withMessage('Este campo é requerido', required)
 
+export const nameValidator = () => ({
+  required: requiredValidator(),
+  minLength: helpers.withMessage(
+    'O nome deve ter pelo menos 2 caracteres',
+    minLength(2)
+  ),
+})
+
 export const emailValidator = () => ({
   required: requiredValidator(),
   email: helpers.withMessage('O e-mail é inválido', email),
