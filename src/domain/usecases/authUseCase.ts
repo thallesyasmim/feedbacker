9a7fa144e6aee77ae3b93737fa4375f06fcf36fb
thalles <thallesgabriel1307@gmail.com>
2022-05-09 18:06:25 -0300

    chore: inject TokenGeneratorRepository and create register method

--- src/domain/usecases/authUseCase.ts
+++ src/domain/usecases/authUseCase.ts
@@ -1,25 +1,46 @@
 import {
-  AuthRepository,
-  AuthRepositoryFactory,
+  TokenGeneratorRepository,
+  TokenGeneratorRepositoryFactory,
+  CreateAccountRepository,
+  CreateAccountRepositoryFactory,
+  CreateAccountDTO,
   LoginDTO,
-} from '@/infra/repositories/authRepository'
+} from '@/infra/repositories'
 
 export class AuthUseCase {
-  private authRepository: AuthRepository
+  private tokenGeneratorRepository: TokenGeneratorRepository
+  private createAccountRepository: CreateAccountRepository
 
-  constructor(authRepository: AuthRepository) {
-    this.authRepository = authRepository
+  constructor(
+    tokenGeneratorRepository: TokenGeneratorRepository,
+    createAccountRepository: CreateAccountRepository
+  ) {
+    this.tokenGeneratorRepository = tokenGeneratorRepository
+    this.createAccountRepository = createAccountRepository
   }
 
   async login(email: string, password: string): Promise<LoginDTO> {
-    const token = await this.authRepository.login(email, password)
+    const token = await this.tokenGeneratorRepository.generate(email, password)
 
     return token
+  }
+
+  async register({ name, email, password }: CreateAccountDTO): Promise<any> {
+    const account = await this.createAccountRepository.create({
+      name,
+      email,
+      password,
+    })
+
+    return account
   }
 }
 
 export class AuthUseCaseFactory {
   static getInstance() {
-    return new AuthUseCase(AuthRepositoryFactory.getInstance())
+    return new AuthUseCase(
+      TokenGeneratorRepositoryFactory.getInstance(),
+      CreateAccountRepositoryFactory.getInstance()
+    )
   }
 }
