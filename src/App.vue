a7e4650e68ee25bb84e17dfb288b70e31a4e7c6a
thalles <thallesgabriel1307@gmail.com>
2022-05-10 19:09:04 -0300

    chore: verify if route is authenticated and token is set

--- src/App.vue
+++ src/App.vue
@@ -4,5 +4,22 @@
 </template>
 
 <script setup lang="ts">
+import { watch } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
 import ModalFactory from '@/components/ModalFactory/index.vue'
+
+const router = useRouter()
+const route = useRoute()
+
+watch(
+  () => route.path,
+  () => {
+    if (route.meta.hasAuth) {
+      const token = window.localStorage.getItem('token')
+      if (!token) {
+        router.push({ name: 'Home' })
+      }
+    }
+  }
+)
 </script>
