8233166cda7fe49fa649ac6a4632b84d27f0a8a0
thalles <thallesgabriel1307@gmail.com>
2022-05-10 19:01:53 -0300

    chore: add request interceptor for set token

--- src/utils/helpers/httpClient.ts
+++ src/utils/helpers/httpClient.ts
@@ -1,7 +1,21 @@
 import axios from 'axios'
+import router from '@/router'
 
 const httpClient = axios.create({
   baseURL: process.env.VUE_APP_BASE_URL || 'http://localhost:3333',
+})
+
+httpClient.interceptors.request.use((config) => {
+  const token = window.localStorage.getItem('token')
+
+  if (token) {
+    config.headers = {
+      Authorization: `Bearer ${token}`,
+      Accept: 'application/json',
+    }
+  }
+
+  return config
 })
 
 httpClient.interceptors.response.use(
@@ -13,6 +27,10 @@ httpClient.interceptors.response.use(
 
     if (status === 0 || status === 500) {
       throw new Error(error)
+    }
+
+    if (status === 401) {
+      router.push({ name: 'Home' })
     }
 
     return error
