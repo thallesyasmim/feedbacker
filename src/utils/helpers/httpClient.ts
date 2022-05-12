82c647266243739a9acacd8c87e51c65ac1d8267
thalles <thallesgabriel1307@gmail.com>
2022-05-12 18:34:42 -0300

    chore: set loading with store

--- src/utils/helpers/httpClient.ts
+++ src/utils/helpers/httpClient.ts
@@ -1,11 +1,13 @@
 import axios from 'axios'
 import router from '@/router'
+import { setGlobalLoading } from '@/store/global'
 
 const httpClient = axios.create({
   baseURL: process.env.VUE_APP_BASE_URL || 'http://localhost:3333',
 })
 
 httpClient.interceptors.request.use((config) => {
+  setGlobalLoading(true)
   const token = window.localStorage.getItem('token')
 
   if (token) {
@@ -19,13 +21,17 @@ httpClient.interceptors.request.use((config) => {
 })
 
 httpClient.interceptors.response.use(
-  (response) => response,
+  (response) => {
+    setGlobalLoading(false)
+    return response
+  },
   (error) => {
     const {
       response: { status },
     } = error
 
     if (status === 0 || status === 500) {
+      setGlobalLoading(false)
       throw new Error(error)
     }
 
