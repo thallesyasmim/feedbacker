8eb4f1f8c82b646c3b75c0067cecd6ea39bd2753
thalles <thallesgabriel1307@gmail.com>
2022-05-16 20:59:39 -0300

    refactor: add types for store and store modules

--- src/types/store.ts
+++ src/types/store.ts
@@ -1,4 +1,11 @@
-export enum StoreModules {
+import Store, { GlobalModule, UserModule } from '@/store'
+
+enum StoreModules {
   USER = 'User',
   GLOBAL = 'Global',
 }
+type IStoreUserModule = typeof UserModule
+type IStoreGlobalModule = typeof GlobalModule
+type IUseStore = IStoreUserModule | IStoreGlobalModule | typeof Store
+
+export { StoreModules, IStoreUserModule, IStoreGlobalModule, IUseStore }
