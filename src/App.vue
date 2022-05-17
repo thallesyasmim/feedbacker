90a47f95f5d1b4fa46d96937095b6f883e2f91fa
thalles <thallesgabriel1307@gmail.com>
2022-05-16 21:09:30 -0300

    chore: set current user with user module in store

--- src/App.vue
+++ src/App.vue
@@ -6,19 +6,29 @@
 <script setup lang="ts">
 import { watch } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
+import { useGetCurrentUser } from '@/composables'
+import { GetCurrentUserUseCaseFactory } from '@/domain/usecases/getCurrentUserUseCase'
 import ModalFactory from '@/components/ModalFactory/index.vue'
+import { IUser } from './types'
+import { setCurrentUser } from './store/user'
 
 const router = useRouter()
 const route = useRoute()
+const { handleGetCurrentUser } = useGetCurrentUser(
+  GetCurrentUserUseCaseFactory.getInstance()
+)
 
 watch(
   () => route.path,
-  () => {
+  async () => {
     if (route.meta.hasAuth) {
       const token = window.localStorage.getItem('token')
       if (!token) {
         router.push({ name: 'Home' })
       }
+
+      const user: IUser = await handleGetCurrentUser()
+      setCurrentUser(user)
     }
   }
 )
