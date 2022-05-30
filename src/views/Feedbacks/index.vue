c9d5d2f994e82e96621e85652a2f2c042b72d35e
thallesyasmim <thallesgabriel1307@gmail.com>
2022-05-29 22:48:58 -0300

    chore(component): initial structure of feedbacks page

--- src/views/Feedbacks/index.vue
+++ src/views/Feedbacks/index.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>Feedbacks</h1>
+  <div class="flex justify-center w-full h-28 bg-brand-main">
+    <header-logged />
+  </div>
+
+  <div class="flex flex-col items-center justify-center h-64 bg-brand-gray">
+    <h1 class="text-4xl font-black text-gray-800">Feedbacks</h1>
+    <p class="mt-1 text-lg text-center text-gray-800 font-regular">
+      Detalhes de todos os feedbacks recebidos
+    </p>
+  </div>
 </template>
+
+<script setup lang="ts">
+import HeaderLogged from '@/components/HeaderLogged/index.vue'
+</script>
