fa5edf848151a09e03458a6f00de846a370219d8
thallesyasmim <thallesgabriel1307@gmail.com>
2022-05-29 22:47:38 -0300

    refactor(component): add margin top

--- src/views/Credentials/index.vue
+++ src/views/Credentials/index.vue
@@ -5,7 +5,7 @@
 
   <div class="flex flex-col items-center justify-center h-64 bg-brand-gray">
     <h1 class="text-4xl font-black text-gray-800">Credenciais</h1>
-    <p class="text-lg text-center text-gray-800 font-regular">
+    <p class="mt-1 text-lg text-center text-gray-800 font-regular">
       Guia de instalação e geração de suas credenciais
     </p>
   </div>
