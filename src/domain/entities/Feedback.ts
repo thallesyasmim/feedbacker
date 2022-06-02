26fa94039a36cd372fbe1504144cd004ac1e0952
thalles <thallesgabriel1307@gmail.com>
2022-06-01 21:15:45 -0300

    chore: export labels

--- src/domain/entities/Feedback.ts
+++ src/domain/entities/Feedback.ts
@@ -14,3 +14,30 @@ export interface Feedback {
   page: string
   createdAt: number
 }
+
+export const LABELS = {
+  all: {
+    label: 'Todos',
+    text: 'text-brand-info',
+    bg: 'bg-brand-info',
+    amount: 0,
+  },
+  issue: {
+    label: 'Problemas',
+    text: 'text-brand-danger',
+    bg: 'bg-brand-danger',
+    amount: 0,
+  },
+  idea: {
+    label: 'Ideias',
+    text: 'text-brand-warning',
+    bg: 'bg-brand-warning',
+    amount: 0,
+  },
+  other: {
+    label: 'Outros',
+    text: 'text-brand-graydark',
+    bg: 'bg-brand-graydark',
+    amount: 0,
+  },
+}
