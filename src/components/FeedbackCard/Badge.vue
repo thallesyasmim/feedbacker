aeeda6ad828babd7eea86a31a2d4b5c0f6d92d40
thalles <thallesgabriel1307@gmail.com>
2022-06-03 21:06:04 -0300

    feat(component): add FeedbackCardBadge

---
+++ src/components/FeedbackCard/Badge.vue
@@ -0,0 +1,38 @@
+<template>
+  <span
+    :class="`bg-${typeComputed.bg}`"
+    class="p-2 text-xs font-black text-white uppercase rounded-full"
+  >
+    {{ typeComputed.label }}
+  </span>
+</template>
+
+<script setup lang="ts">
+import { defineProps, computed } from 'vue'
+
+interface IProps {
+  type: string
+}
+
+const props = defineProps<IProps>()
+
+const typeComputed = computed(() => {
+  switch (props.type.toLowerCase()) {
+    case 'issue':
+      return {
+        label: 'problema',
+        bg: 'brand-danger',
+      }
+    case 'idea':
+      return {
+        label: 'ideia',
+        bg: 'brand-warning',
+      }
+    default:
+      return {
+        label: 'outros',
+        bg: 'brand-graydark',
+      }
+  }
+})
+</script>
