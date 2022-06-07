b7cbcf0eb925eb0c31e1fd76745e14bf103d926b
thalles <thallesgabriel1307@gmail.com>
2022-06-07 19:02:53 -0300

    feat(component): add Feedbacks page

--- src/views/Feedbacks/index.vue
+++ src/views/Feedbacks/index.vue
@@ -9,8 +9,181 @@
       Detalhes de todos os feedbacks recebidos
     </p>
   </div>
+
+  <div class="flex justify-center w-full pb-20">
+    <div class="w-4/5 max-w-6xl py-10 grid grid-cols-4 gap-2">
+      <div>
+        <h1 class="text-3xl font-black text-brand-darkgray">Listagem</h1>
+        <suspense>
+          <template #default>
+            <filters
+              @select="handleChangeFeedbacksType"
+              class="mt-8 animate__animated animate__fadeIn animate__faster"
+            />
+          </template>
+          <template #fallback>
+            <filters-loading class="mt-8" />
+          </template>
+        </suspense>
+      </div>
+
+      <div class="px-10 pt-20 col-span-3">
+        <p
+          v-if="state.hasError"
+          class="text-lg text-center text-gray-800 font-regular"
+        >
+          Aconteceu um erro ao carregar os feedbacks 🥺
+        </p>
+        <p
+          v-if="
+            !state.feedbacks.length &&
+            !state.isLoading &&
+            !state.isLoadingFeedbacks &&
+            !state.hasError
+          "
+          class="text-lg text-center text-gray-800 font-regular"
+        >
+          Ainda nenhum feedback recebido 🤓
+        </p>
+
+        <feedback-card-loading
+          v-if="state.isLoading || state.isLoadingFeedbacks"
+        />
+        <feedback-card
+          v-else
+          v-for="(feedback, index) in state.feedbacks"
+          :key="feedback.id"
+          :is-opened="index === 0"
+          :feedback="feedback"
+          class="mb-8"
+        />
+        <feedback-card-loading v-if="state.isLoadingMoreFeedbacks" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { onErrorCaptured, onMounted, onUnmounted, reactive } from 'vue'
+import { useFeedbacks } from '@/main/factories/composables/useFeedbacksFactory'
+import { Feedback, FeedbackType } from '@/domain/entities/Feedback'
+import Filters from './Filters.vue'
+import FiltersLoading from './FiltersLoading.vue'
 import HeaderLogged from '@/components/HeaderLogged/index.vue'
+import FeedbackCard from '@/components/FeedbackCard/index.vue'
+import FeedbackCardLoading from '@/components/FeedbackCard/Loading.vue'
+
+interface IState {
+  isLoading: boolean
+  isLoadingFeedbacks: boolean
+  isLoadingMoreFeedbacks: boolean
+  feedbacks: Feedback[]
+  currentFeedbackType: FeedbackType
+  pagination: {
+    limit: number
+    offset: number
+    total: number
+  }
+  hasError: boolean
+}
+
+const state = reactive<IState>({
+  isLoading: false,
+  isLoadingFeedbacks: false,
+  isLoadingMoreFeedbacks: false,
+  feedbacks: [],
+  currentFeedbackType: '',
+  pagination: {
+    limit: 5,
+    offset: 0,
+    total: 0,
+  },
+  hasError: false,
+})
+
+const { handleFeedbacks } = useFeedbacks()
+
+onErrorCaptured(handleErrors)
+
+onMounted(() => {
+  handleFeedbacksAdapter()
+  window.addEventListener('scroll', handleScroll, false)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', handleScroll, false)
+})
+
+function handleErrors(error: unknown) {
+  state.isLoading = false
+  state.isLoadingFeedbacks = false
+  state.isLoadingMoreFeedbacks = false
+  state.hasError = !!error
+}
+
+async function handleFeedbacksAdapter() {
+  try {
+    state.isLoading = true
+
+    const data = await handleFeedbacks({
+      ...state.pagination,
+      type: state.currentFeedbackType,
+    })
+
+    state.feedbacks = data.results
+    state.pagination = data.pagination
+    state.isLoading = false
+  } catch (error) {
+    handleErrors(error)
+  }
+}
+
+async function handleChangeFeedbacksType(type: FeedbackType) {
+  try {
+    state.isLoadingFeedbacks = true
+    state.pagination.limit = 5
+    state.pagination.offset = 0
+    state.currentFeedbackType = type
+
+    const data = await handleFeedbacks({
+      ...state.pagination,
+      type: state.currentFeedbackType,
+    })
+
+    state.feedbacks = data.results
+    state.pagination = data.pagination
+    state.isLoadingFeedbacks = false
+  } catch (error) {
+    handleErrors(error)
+  }
+}
+
+async function handleScroll() {
+  const isBottomOfWindow =
+    Math.ceil(document.documentElement.scrollTop + window.innerHeight) >=
+    document.documentElement.scrollHeight
+  if (state.isLoading || state.isLoadingMoreFeedbacks) return
+  if (!isBottomOfWindow) return
+  if (state.feedbacks.length >= state.pagination.total) return
+
+  try {
+    state.isLoadingMoreFeedbacks = true
+
+    const data = await handleFeedbacks({
+      ...state.pagination,
+      type: state.currentFeedbackType,
+      offset: state.pagination.offset + 5,
+    })
+
+    if (data.results.length) {
+      state.feedbacks.push(...data.results)
+    }
+
+    state.pagination = data.pagination
+  } catch (error) {
+    handleErrors(error)
+  } finally {
+    state.isLoadingMoreFeedbacks = false
+  }
+}
 </script>
